07696d2107e5649e7cdffe329215ee48b69dda88
LukeHSM <lucashsmartins94@gmail.com>
2024-08-01 21:05:26 -0300

    mudanças2

--- Aula-Java-Basico/src/App.java
+++ Aula-Java-Basico/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
+    
+        //Meu nome é Biruleibe
+        // exitclsSou um conquistador
+        
         System.out.println("Hello, World!");
     }
 }
