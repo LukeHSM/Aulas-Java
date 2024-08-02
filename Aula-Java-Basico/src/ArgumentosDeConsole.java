e520b7ba6a3237a4aa8ddd1f78d78323bad79feb
LukeHSM <lucashsmartins94@gmail.com>
2024-08-01 21:53:04 -0300

    main args

---
+++ Aula-Java-Basico/src/ArgumentosDeConsole.java
@@ -0,0 +1,13 @@
+public class ArgumentosDeConsole {
+    public static void main(String[] args) throws Exception {
+        String nome = args[0];
+        int idade = Integer.valueOf(args[1]);
+        String cidade = args[2];
+        double altura = Double.valueOf(args[3]); // Digite com ponto, e não vírgula.
+
+        System.out.printf("Oi, meu nome é %s!%n", nome);
+        System.out.printf("Eu tenho %d anos de idade.%n", idade);
+        System.out.printf("Eu moro em %s.%n", cidade);
+        System.out.printf("E tenho %.2f m de altura.%n", altura);
+    }
+}
