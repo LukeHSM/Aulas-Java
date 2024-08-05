8a5def0af310d3ac79561a80ece13353848c2057
LukeHSM <lucashsmartins94@gmail.com>
2024-08-04 23:10:35 -0300

    Terminando o Operacoes.java

--- Aula-Java-Basico/src/Operacoes.java
+++ Aula-Java-Basico/src/Operacoes.java
@@ -4,34 +4,45 @@ public class Operacoes {
 
     static void efetuarOperacao(){
         
-        // Refatorar? (colocar esse início em um método específico?) - Daqui...
         Scanner read = new Scanner(System.in);
-        System.out.print("Digite o número A: ");
+
+        System.out.print("\n\nDigite o número A: ");
         int num1 = Integer.valueOf(read.nextInt());
+
         System.out.print("Digite o número B: ");
         int num2 = Integer.valueOf(read.nextInt());
+
         System.out.printf("Digite a letra da opção desejada:%nADIÇÃO (A)%nSUBTRAÇÃO (S)%nMultiplicação (M)%nDIVISÃO (D)%n>>> ");
+
         String operacao = read.next().toUpperCase();
-        // ...Até aqui? tem como retornar dois tipos diferentes? "return num1, num2, operacao"????
 
         switch (operacao) {
             case "A":
-                System.out.printf("%n%d + %d = %.1f", num1, num2, (float)num1+num2);
-                // Criar um método (Em cada CASE, perguntar se quer fazer outra operacao e reiniciar esse método atual)????
-                break;
+                System.out.printf("%n%d + %d = %.1f", num1, num2, (float)num1+num2);break;
             case "M":
-                System.out.printf("%n%d * %d = %.1f", num1, num2, (float)num1*num2);
-                break;
+                System.out.printf("%n%d * %d = %.1f", num1, num2, (float)num1*num2);break;
             case "D":
-                System.out.printf("%n%d / %d = %.1f", num1, num2, (float)num1/num2);
-                break;
+                System.out.printf("%n%d / %d = %.1f", num1, num2, (float)num1/num2);break;
             case "S":
-                System.out.printf("%n%d - %d = %.1f", num1, num2, (float)num1-num2);
-                break;
+                System.out.printf("%n%d - %d = %.1f", num1, num2, (float)num1-num2);break;
             default:
                 System.out.println("\nOPERAÇÃO INCORRETA!!!\n\n");
-                efetuarOperacao();
-                break;
+                efetuarOperacao();break;
+        }
+
+        // Perguntando se quer continuar...
+        // Por algum motivo, não funcionou com o IF.
+        System.out.printf("%n%nRealizar outra operação?%n: ");
+
+        String continuar = read.next().toUpperCase();
+
+        switch (continuar) {
+            case "S":
+                efetuarOperacao();break;
+            case "SIM":
+                efetuarOperacao();break;
+            case "YES":
+                efetuarOperacao();break;
         }
 
     }
