03ee0152944b8238f487af48df13c87bcb0d55b4
LukeHSM <lucashsmartins94@gmail.com>
2024-08-13 21:37:11 -0300

    Aperfeiçoando a lógica de programação

--- Aula-Java-Basico/src/ArgumentosDeConsole.java
+++ Aula-Java-Basico/src/ArgumentosDeConsole.java
@@ -4,17 +4,23 @@
  * @since August 8th, 2024
  * @apiNote No console, na pasta bin: java ArgumentosDeConsole nome idade cidade/estado altura[em metros]
 */
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ArgumentosDeConsole {
     public static void main(String[] args) throws Exception {
-        String nome = args[0];
-        int idade = Integer.valueOf(args[1]);
-        String cidade = args[2];
-        double altura = Double.valueOf(args[3]); // Digite com ponto, e não vírgula.
+        Scanner read = new Scanner(System.in).useLocale(Locale.US);
+        System.out.print("Digite seu nome: ");
+        String nome = read.next();
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = read.next();
+        System.out.print("Digite sua idade: ");
+        int idade = read.nextInt();
+        System.out.print("Digite sua altura: "); // em metros
+        double altura = read.nextDouble(); // Digite com ponto, e não vírgula.
 
-        System.out.printf("Oi, meu nome é %s!%n", nome);
+        System.out.printf("Oi, meu nome é %s %s!%n", nome, sobrenome);
         System.out.printf("Eu tenho %d anos de idade.%n", idade);
-        System.out.printf("Eu moro em %s.%n", cidade);
         System.out.printf("E tenho %.2f m de altura.%n", altura);
     }
 }
